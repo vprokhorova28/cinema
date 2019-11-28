fcebe24a2b5f6ec4ba2637a3226a37055ad0c5db
vprokhorova28 <55251241+vprokhorova28@users.noreply.github.com>
2019-11-28 20:12:43 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,3 +1,7 @@
 from cinemaparse import CinemaParser
 
 spb_parser = CinemaParser('spb')
+spb_parser.extract_raw_content()
+spb_parser.print_raw_content()
+print(spb_parser.get_films_list())
+print(spb_parser.get_film_nearest_session('Джокер'))
